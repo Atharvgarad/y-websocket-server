cb82bbf366fc26e6cd796c58093dceacf1f75190
Atharv Garad <atharvgarad12@gmail.com>
2026-04-16 17:28:54 +0530

    Refactor WebSocket server setup and imports

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const { createServer } = require('http')
-const { setupWSConnection } = require('y-websocket/bin/utils')
-const { WebSocketServer } = require('ws')
+const http = require('http')
+const WebSocket = require('ws')
+const { setupWSConnection } = require('y-websocket/bin/utils.cjs')
 
-const server = createServer((req, res) => {
+const server = http.createServer((req, res) => {
   res.writeHead(200)
   res.end('y-websocket server running')
 })
 
-const wss = new WebSocketServer({ server })
+const wss = new WebSocket.Server({ server })
 
 wss.on('connection', (ws, req) => {
   setupWSConnection(ws, req)
